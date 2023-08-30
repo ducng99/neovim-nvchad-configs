86889bd0e8b885aa8f63c91b5625b63b7fe961eb
ducng99 <tom@ducng.dev>
2023-08-30 20:58:23 +1200

    Wed Aug 30 20:58:23 NZST 2023

--- plugins.lua
+++ plugins.lua
@@ -19,8 +19,12 @@ local plugins = {
     end,
   },
   {
-    "github/copilot.vim",
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
     event = "InsertEnter",
+    config = function()
+      require("copilot").setup {}
+    end,
   },
   {
     "nvim-telescope/telescope.nvim",
